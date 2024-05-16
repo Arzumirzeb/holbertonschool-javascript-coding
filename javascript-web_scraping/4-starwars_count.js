367bf86469acf61fc36e6f9e6099c181841070e8
arzumirzeb <arzumirzeb03@gmail.com>
2024-05-16 20:41:17 +0400

    4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,13 +6,13 @@ const request = require('request');
 const apiUrl = process.argv[2];
 
 request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
+  if (error) throw error
+  let count = 0;
   const films = JSON.parse(body).results;
-  const moviesWithWedgeAntilles = films.filter((film) => film.characters.includes(`https://swapi-api.hbtn.io/api/people/18/`));
-  console.log(moviesWithWedgeAntilles.length);
+  films.forEach((film) => {
+    if (film.characters.includes('/18/')) {
+      count++;
+    }
+  });
+  console.log(count);
 });
-
